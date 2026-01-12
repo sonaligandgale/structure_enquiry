efafaec970b381abe9ceb1be13d234db5a4df2d0
sonaligandgale <sonaligandgalekar@gmail.com>
2026-01-12 15:22:42 +0530

    Update hotel_billing.py

--- hotel_billing.py
+++ hotel_billing.py
@@ -1,45 +1,31 @@
 # Program to calculate hotel room bill
 
-import sys
+import os
 
 def calculate_bill(room_type, days):
-    if room_type.lower() == "standard":
-        rate = 1500
-    elif room_type.lower() == "deluxe":
-        rate = 2500
-    elif room_type.lower() == "suite":
-        rate = 4000
-    else:
-        return -1
+    rates = {
+        "standard": 2000,
+        "deluxe": 3000,
+        "suite": 4000
+    }
+
+    rate = rates.get(room_type.lower(), 2000)
     return rate * days
 
 
 if __name__ == "__main__":
     print("=== Hotel Room Billing System ===")
 
-    try:
-        # Command line input
-        if len(sys.argv) == 4:
-            name = sys.argv[1]
-            room_type = sys.argv[2]
-            days = int(sys.argv[3])
+    # Read values from Jenkins environment variables
+    name = os.getenv("customer_name", "Guest")
+    room_type = os.getenv("room_type", "standard")
+    days = int(os.getenv("days_stayed", "1"))
 
-        # User input
-        else:
-            name = input("Enter Customer Name: ")
-            room_type = input("Enter Room Type (Standard/Deluxe/Suite): ")
-            days = int(input("Enter Number of Days Stayed: "))
+    total_bill = calculate_bill(room_type, days)
 
-        total_bill = calculate_bill(room_type, days)
+    print("\n--- BILL DETAILS ---")
+    print("Customer Name :", name)
+    print("Room Type     :", room_type)
+    print("Days Stayed   :", days)
+    print("Total Bill    : ₹", total_bill)
 
-        if total_bill == -1:
-            print("Invalid room type entered.")
-        else:
-            print("\n--- BILL DETAILS ---")
-            print("Customer Name :", name)
-            print("Room Type     :", room_type)
-            print("Days Stayed   :", days)
-            print("Total Bill    : ₹", total_bill)
-
-    except ValueError:
-        print("Invalid input. Please enter valid numeric values.")
